4bb8bb873852e48f9a6275f426f9fda819f66685
julianacholder <j.holder@alustudent.com>
2024-09-14 12:03:10 -0700

    1 and 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-""" defines function that calculates the shape of a matrix """
+'''
+    A function def matrix_shape(matrix): that calculates the shape of a matrix
+'''
 
 
 def matrix_shape(matrix):
-    """ returns list of integers representing dimensions of given matrix """
-    matrix_shape = []
-    while (type(matrix) is list):
-        matrix_shape.append(len(matrix))
+    '''
+        Calculates the shape of a matrix
+    '''
+    mat_shape = []
+    while isinstance(matrix, list):
+        mat_shape.append(len(matrix))
         matrix = matrix[0]
-    return matrix_shape
+    return mat_shape
